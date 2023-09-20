836bacf98addd8b4ebeae481f8fc92aa9b42e316
reyhanmichiels <michiels.reyhan@gmail.com>
2023-09-20 20:30:17 +0700

    test(Registration): create RegistrationUsecase mock

--- app/user/usecase/user_usecase_mock.go
+++ app/user/usecase/user_usecase_mock.go
@@ -1,1 +1,23 @@
 package usecase
+
+import (
+	"github.com/reyhanmichiels/bring_coffee/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type UserUsecaseMock struct {
+	Mock mock.Mock
+}
+
+func (userUsecase *UserUsecaseMock) RegistrationUsecase(request domain.RegistBind) (string, interface{}) {
+	args := userUsecase.Mock.Called(request)
+	if args[1] != nil {
+		return "", args[1]
+	}
+
+	return args[0].(string), nil
+}
+
+func (userUsecase *UserUsecaseMock) VerifyAccountUsecase(request domain.VerifyAccountBind) interface{} {
+	return nil
+}
