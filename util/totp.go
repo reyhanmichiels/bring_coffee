ae64d26f821cd6b1afe1c1429e3022cb0644cfc2
reyhanmichiels <michiels.reyhan@gmail.com>
2023-09-20 14:04:03 +0700

    fix: remove unused package

--- util/totp.go
+++ util/totp.go
@@ -1,10 +1,13 @@
 package util
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/pquerna/otp/totp"
+	"gopkg.in/gomail.v2"
 )
 
 var validateOptions = totp.ValidateOpts{
@@ -28,4 +31,32 @@ func ValidateOTP(code string) (bool, error) {
 	}
 
 	return ok, nil
+}
+
+func SendOTP(name, email, code string) error {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", "Bring Coffee")
+	mailer.SetHeader("To", email)
+	mailer.SetHeader("Subject", "Your OTP Code")
+	body := fmt.Sprintf("Hello %s, Welcome to Bring Coffee\n This is your OTP Code\n%s\nThis code only valid for 5 minutes", name, code)
+	mailer.SetBody("text/html", body)
+
+	smtpPort, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		return err
+	}
+
+	dialer := gomail.NewDialer(
+		os.Getenv("SMTP_HOST"),
+		smtpPort,
+		os.Getenv("SMTP_EMAIL"),
+		os.Getenv("SMTP_PASSWORD"),
+	)
+
+	err = dialer.DialAndSend(mailer)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
