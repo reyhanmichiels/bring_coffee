a6c1e3a3d683da5afc1d76477fd4f6dc17241b7d
reyhanmichiels <michiels.reyhan@gmail.com>
2023-09-23 14:39:29 +0700

    fix: add allow origin in cors middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -3,7 +3,7 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 func CORS(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
