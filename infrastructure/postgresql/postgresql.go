c66e73eb7971470d0227e2f492d91524814f59ac
reyhanmichiels <michiels.reyhan@gmail.com>
2023-09-23 13:38:43 +0700

    build: make docker environment

--- infrastructure/postgresql/postgresql.go
+++ infrastructure/postgresql/postgresql.go
@@ -12,15 +12,20 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
+	port := os.Getenv("DB_DOCKER_PORT")
+	if os.Getenv("APP") == "local" {
+		port = os.Getenv("DB_PORT")
+	}
+
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"))
+		port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-        log.Println(err)
+		log.Println(err)
 		log.Fatal("DB connection error")
 	}
 
